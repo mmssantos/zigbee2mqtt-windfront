7fa55e56311d8bc01ad75ac78fc797bc81e3bc50
Nerivec <62446222+Nerivec@users.noreply.github.com>
2025-05-06 14:34:28 +0200

    fix: minor cleanup

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,6 +2,28 @@ import i18n, { type ResourceLanguage } from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import { register } from "timeago.js";
+import timeBg from "timeago.js/lib/lang/bg.js";
+import timeCa from "timeago.js/lib/lang/ca.js";
+import timeCs from "timeago.js/lib/lang/cs.js";
+import timeDa from "timeago.js/lib/lang/da.js";
+import timeDe from "timeago.js/lib/lang/de.js";
+import timeEs from "timeago.js/lib/lang/es.js";
+import timeEu from "timeago.js/lib/lang/eu.js";
+import timeFi from "timeago.js/lib/lang/fi.js";
+import timeFR from "timeago.js/lib/lang/fr.js";
+import timeHu from "timeago.js/lib/lang/hu.js";
+import timeIt from "timeago.js/lib/lang/it.js";
+import timeKo from "timeago.js/lib/lang/ko.js";
+import timeNo from "timeago.js/lib/lang/nb_NO.js";
+import timeNl from "timeago.js/lib/lang/nl.js";
+import timePl from "timeago.js/lib/lang/pl.js";
+import timePtBr from "timeago.js/lib/lang/pt_BR.js";
+import timeRu from "timeago.js/lib/lang/ru.js";
+import timeSv from "timeago.js/lib/lang/sv.js";
+import timeTr from "timeago.js/lib/lang/tr.js";
+import timeUa from "timeago.js/lib/lang/uk.js";
+import timeChs from "timeago.js/lib/lang/zh_CN.js";
+import timeZh from "timeago.js/lib/lang/zh_TW.js";
 import bgTranslations from "./locales/bg.json" with { type: "json" };
 import caTranslations from "./locales/ca.json" with { type: "json" };
 import chsTranslations from "./locales/chs.json" with { type: "json" };
@@ -26,53 +48,30 @@ import trTranslations from "./locales/tr.json" with { type: "json" };
 import uaTranslations from "./locales/ua.json" with { type: "json" };
 import zhTranslations from "./locales/zh.json" with { type: "json" };
 
-import timeBg from "timeago.js/lib/lang/bg";
-import timeCa from "timeago.js/lib/lang/ca";
-import timeCs from "timeago.js/lib/lang/cs";
-import timeDa from "timeago.js/lib/lang/da";
-import timeDe from "timeago.js/lib/lang/de";
-import timeEs from "timeago.js/lib/lang/es";
-import timeEu from "timeago.js/lib/lang/eu";
-import timeFi from "timeago.js/lib/lang/fi";
-import timeFR from "timeago.js/lib/lang/fr";
-import timeHu from "timeago.js/lib/lang/hu";
-import timeIt from "timeago.js/lib/lang/it";
-import timeKo from "timeago.js/lib/lang/ko";
-import timeNo from "timeago.js/lib/lang/nb_NO";
-import timeNl from "timeago.js/lib/lang/nl";
-import timePl from "timeago.js/lib/lang/pl";
-import timePtBr from "timeago.js/lib/lang/pt_BR";
-import timeRu from "timeago.js/lib/lang/ru";
-import timeSv from "timeago.js/lib/lang/sv";
-import timeTr from "timeago.js/lib/lang/tr";
-import timeUa from "timeago.js/lib/lang/uk";
-import timeChs from "timeago.js/lib/lang/zh_CN";
-import timeZh from "timeago.js/lib/lang/zh_TW";
-
+register("bg", timeBg);
 register("ca", timeCa);
-register("pl", timePl);
-register("fr", timeFR);
+register("cs", timeCs);
+register("da", timeDa);
 register("de", timeDe);
-register("ru", timeRu);
-register("ptbr", timePtBr);
 register("es", timeEs);
-register("ua", timeUa);
-register("zh-CN", timeChs);
-register("nl", timeNl);
+register("eu", timeEu);
+register("fi", timeFi);
+register("fr", timeFR);
+register("hu", timeHu);
 register("it", timeIt);
 register("ko", timeKo);
-register("zh", timeZh);
-register("cs", timeCs);
-register("fi", timeFi);
+register("nl", timeNl);
+register("no", timeNo);
+register("pl", timePl);
+register("ptbr", timePtBr);
+register("ru", timeRu);
 register("sv", timeSv);
 register("tr", timeTr);
-register("no", timeNo);
-register("da", timeDa);
-register("bg", timeBg);
-register("hu", timeHu);
-register("eu", timeEu);
+register("ua", timeUa);
+register("zh-CN", timeChs);
+register("zh", timeZh);
 
-export const resources = {
+const resources = {
     ca: caTranslations as ResourceLanguage,
     en: enTranslations as ResourceLanguage,
     fr: frTranslations as ResourceLanguage,
@@ -99,9 +98,11 @@ export const resources = {
 } as const;
 
 const debug = process.env.NODE_ENV !== "production";
+
 i18n.on("languageChanged", (lng: string) => {
     document.documentElement.lang = lng;
 });
+
 i18n.use(LanguageDetector)
     .use(initReactI18next)
     .init({
@@ -115,7 +116,9 @@ i18n.use(LanguageDetector)
     });
 
 const currentLanguage = i18n.language.split("-")[0].toLocaleLowerCase();
+
 if (!resources[currentLanguage]) {
     i18n.changeLanguage("en");
 }
+
 export default i18n;
