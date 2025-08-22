e2aa5cd5ee2baa585306bd4b964ea9d6da414381
Nerivec <62446222+Nerivec@users.noreply.github.com>
2025-08-22 10:21:20 +0200

    fix: display build in dec for OTA file version

--- src/components/ota-page/index.ts
+++ src/components/ota-page/index.ts
@@ -5,9 +5,9 @@ export const formatOtaFileVersion = (version: number | null | undefined) => {
 
     const versionString = version.toString(16).padStart(8, "0");
     const appRelease = `${versionString[0]}.${versionString[1]}`;
-    const appBuild = versionString.slice(2, 4);
+    const appBuild = Number.parseInt(versionString.slice(2, 4), 16);
     const stackRelease = `${versionString[4]}.${versionString[5]}`;
-    const stackBuild = versionString.slice(6);
+    const stackBuild = Number.parseInt(versionString.slice(6), 16);
 
     return [appRelease, appBuild, stackRelease, stackBuild];
 };
