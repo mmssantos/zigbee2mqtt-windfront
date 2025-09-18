dc2453a597c0e5119c55fa8e6cac01d26f4d8252
Márcio Santos <marcio.santos@aminha.cloud>
2025-09-18 19:31:53 +0100

    Renamed NGX_DNS_RESOLVER variable to NGINX_RESOLVER_ADDRESS. Removed nginx.conf cat to stdout. Removed valid=10s configuration from nginx.conf.

--- .docker/scripts/90-awk-nginx-conf-with-resolv-conf.sh
+++ .docker/scripts/90-awk-nginx-conf-with-resolv-conf.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env sh
 
-# If NGX_DNS_RESOLVER variable is not defined, grab it from /etc/resolv.conf
-if [ "$NGX_DNS_RESOLVER" == "" ]; then
-    export NGX_DNS_RESOLVER=$(awk '/nameserver/{a=(a?a" "$2:$2)} END{print a}' /etc/resolv.conf 2> /dev/null)
+# If NGINX_RESOLVER_ADDRESS variable is not defined, grab it from /etc/resolv.conf
+if [ -z "$NGINX_RESOLVER_ADDRESS" ]; then
+    export NGINX_RESOLVER_ADDRESS=$(awk '/nameserver/{a=(a?a" "$2:$2)} END{print a}' /etc/resolv.conf 2> /dev/null)
 fi
 
-echo "Configured nameserver is: $NGX_DNS_RESOLVER"
+# Output nameserver configuration for informational purposes
+echo "Configured nameserver is: $NGINX_RESOLVER_ADDRESS"
 
 echo "Manipulating nginx.conf template configuration with the gathered data"
 
-# Substitute the NGX_DNS_RESOLVER placeholder of nginx.conf.template with the IP address of the DNS Resolver
-envsubst '$NGX_DNS_RESOLVER' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
-cat /etc/nginx/nginx.conf
+# Substitute the NGINX_RESOLVER_ADDRESS placeholder of nginx.conf.template with the IP address of the DNS Resolver
+envsubst '$NGINX_RESOLVER_ADDRESS' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
