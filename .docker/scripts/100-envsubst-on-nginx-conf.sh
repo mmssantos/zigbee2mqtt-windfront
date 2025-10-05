cedf4e4e1029a0a4791fef631f6ce0dc12c9fe15
Márcio Santos <marcio.santos@aminha.cloud>
2025-10-05 01:03:47 +0100

    If environment variable "NGINX_ENTRYPOINT_LOCAL_RESOLVERS" is not set, hardcoded dns server "127.0.0.11" will be used, if it is set, local dns servers will be grabbed and configured

--- .docker/scripts/100-envsubst-on-nginx-conf.sh
+++ .docker/scripts/100-envsubst-on-nginx-conf.sh
@@ -6,7 +6,9 @@ set -eu
 LC_ALL=C
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-[ "${NGINX_LOCAL_RESOLVERS:-}" ] || return 0
+# If NGINX_LOCAL_RESOLVERS is not set or is empty, assign it the default value of "127.0.0.11".
+: "${NGINX_LOCAL_RESOLVERS:=127.0.0.11}"
+export NGINX_LOCAL_RESOLVERS
 
 # Substitute the variable in the template file and generate the final config.
 # `envsubst` is a standard utility that substitutes environment variables in shell format strings.
--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ FROM nginx:alpine-slim AS prod
 EXPOSE 80
 
 COPY .docker/scripts/ /docker-entrypoint.d/
-COPY .docker/nginx.conf /etc/nginx/
 COPY .docker/nginx.conf.template /etc/nginx/
 
 RUN chmod +x /docker-entrypoint.d/100-envsubst-on-app-envs.sh /docker-entrypoint.d/100-envsubst-on-nginx-conf.sh
